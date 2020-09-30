fabddbd9f2329fa62c90a196c667b0117c5da534
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2020-09-30 06:03:46 -0300

    Tentando usar emoji pra mudar cargo

--- matebot/plugins/nsa20.py
+++ matebot/plugins/nsa20.py
@@ -21,7 +21,7 @@ from discord import utils
 
 cargos = ['Desenvolvedor', 'Designer', 'Negocios', 'Cientista']
 
-async def change_role(ctx):
+async def change_role_callback(ctx):
   await ctx.message.author.add_roles(utils.get(ctx.message.guild.roles,
     name = str(ctx.command).capitalize()))
   for cargo in cargos:
@@ -36,13 +36,50 @@ def add_commands(bot: Bot):
   @bot.command()
   async def dev(ctx):
     ctx.command = 'desenvolvedor'
-    await change_role(ctx)
+    await change_role_callback(ctx)
   @bot.command()
   async def designer(ctx):
-    await change_role(ctx)
+    await change_role_callback(ctx)
   @bot.command()
   async def negocios(ctx):
-    await change_role(ctx)
+    ctx.command = 'negócios'
+    await change_role_callback(ctx)
   @bot.command()
   async def cientista(ctx):
-    await change_role(ctx)
+    await change_role_callback(ctx)
+
+  ## FIXME: Não funciona, não consigo o nome do emoji
+  @bot.event
+  async def on_raw_reaction_add(payload):
+   message_id = payload.message_id
+   ## Mensagem de boas vindas
+   if message_id == 760632139598528562:
+    guild_id = payload.guild_id
+    guild = utils.find(lambda g : g.id == guild_id, bot.guilds)
+    logging.warning(str(payload.emoji))
+    if payload.emoji.name == 'computer':
+      role = utils.get(guild.roles, name = 'Desenvolvedor')
+    elif payload.emoji.name == 'briefcase':
+      role = utils.get(guild.roles, name = 'Negócios')
+    elif payload.emoji.name == 'microscope':
+      role = utils.get(guild.roles, name = 'Cientista')
+    elif payload.emoji.name == 'art':
+      role = utils.get(guild.roles, name = 'Designer')
+    else:
+      role = utils.get(guild.roles, name = payload.emoji.name)
+    if role is not None:
+      member = utils.find(lambda m : m.id == payload.user_id, guild.members)
+      if member is not None:
+        await member.add_roles(role)
+        for cargo in cargos:
+          if role != cargo:
+            await member.remove_roles(utils.get(guild.roles, name = cargo))
+        logging.info(u"{0} mudou o cargo para {1}".format(member, role))
+      else:
+        logging.warning(u"Membro não encontrado.")
+    else:
+      logging.warning(u"Cargo não encontrado.")
+
+  @bot.event
+  async def on_raw_reaction_remove(payload):
+    pass
